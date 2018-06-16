885d7bcaa05d4f027016b01ee7b3d5d0ccab8c93
Christophe Nouguier <christophe.nouguier@free.fr>
2018-06-16 15:39:35 +0200

    Updated kiosk

--- Kiosk.vue
+++ Kiosk.vue
@@ -1,9 +1,33 @@
 <template>
-  <div>
-    KALISIO
-  </div>
+  <carousel>
+    <slide>
+      <img src="https://s3.eu-central-1.amazonaws.com/kalisio-artwork/kalisio/kalisio-logo-256x112.png" style="width: 300px; max-width: 100%;" />
+    </slide>
+    <slide>
+      <img src="https://s3.eu-central-1.amazonaws.com/kalisio-artwork/kaabah/kaabah-logo-256x112.png" style="width: 300px; max-width: 100%;" />
+    </slide>
+    <slide>
+      <img src="https://s3.eu-central-1.amazonaws.com/kalisio-artwork/kargo/kargo-logo-256x112.png" style="width: 300px; max-width: 100%;" />      
+    </slide>
+    <slide>
+      <img src="https://s3.eu-central-1.amazonaws.com/kalisio-artwork/weacast/weacast-logo-256x112.png" style="width: 300px; max-width: 100%;" />      
+    </slide>
+    <slide>
+      <img src="https://s3.eu-central-1.amazonaws.com/kalisio-artwork/kdk/kdk-logo-256x112.png" style="width: 300px; max-width: 100%;" />      
+    </slide>
+    <slide>
+      <img src="https://s3.eu-central-1.amazonaws.com/kalisio-artwork/aktnmap/aktnmap-logo-256x112.png" style="width: 300px; max-width: 100%;" />      
+    </slide>
+  </carousel>
 </template>
 
 <script>
-export default {}
+import { Carousel, Slide } from 'vue-carousel'
+
+export default {
+  components: {
+    Carousel,
+    Slide
+  }
+}
 </script>

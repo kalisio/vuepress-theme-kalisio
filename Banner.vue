cf4395fa46859b03aa9a4c4009cc0908e0c14611
Christophe Nouguier <christophe.nouguier@free.fr>
2018-06-17 23:13:03 +0200

    Updated banner image

--- Banner.vue
+++ Banner.vue
@@ -1,7 +1,7 @@
 <template>
   <p align="center">
     <a href="https://kalisio.com">
-	    <img src="https://s3.eu-central-1.amazonaws.com/kalisioscope/kalisio/kalisio-logo-black-256x256.png" />
+	    <img src="https://s3.eu-central-1.amazonaws.com/kalisioscope/kalisio/kalisio-logo-black-256x84.png" />
     </a>
 	  <br />
 	  <i><small>Unleashing the potential of spatial information</small></i>

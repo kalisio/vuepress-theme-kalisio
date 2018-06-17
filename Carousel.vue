8c7688c1020cb15198fccbe9c855fe627cf20e2b
Christophe Nouguier <christophe.nouguier@free.fr>
2018-06-17 19:14:46 +0200

    SSR test

--- Carousel.vue
+++ Carousel.vue
@@ -1,49 +1,51 @@
 <template>
-  <swiper :options="swiperOption">
-    <!--swiper-slide class="kalisio"></swiper-slide>
-    <swiper-slide class="kaabah"></swiper-slide>
-    <swiper-slide class="kargo"></swiper-slide>
-    <swiper-slide class="weacast"></swiper-slide>
-    <swiper-slide class="krawler"></swiper-slide>
-    <swiper-slide class="kdk"></swiper-slide>
-    <swiper-slide class="aktnmap"></swiper-slide-->
+  <ClientOnly>
+    <swiper :options="swiperOption">
+      <!--swiper-slide class="kalisio"></swiper-slide>
+      <swiper-slide class="kaabah"></swiper-slide>
+      <swiper-slide class="kargo"></swiper-slide>
+      <swiper-slide class="weacast"></swiper-slide>
+      <swiper-slide class="krawler"></swiper-slide>
+      <swiper-slide class="kdk"></swiper-slide>
+      <swiper-slide class="aktnmap"></swiper-slide-->
 
-    <swiper-slide>
-      <a href="https://kalisio.com">
-        <img src="https://s3.eu-central-1.amazonaws.com/kalisioscope/kalisio/kalisio-logo-black-128x128.png" />
-      </a>
-    </swiper-slide>
-    <swiper-slide>
-       <a href="https://kalisio.github.io/kaabah/">
-        <img src="https://s3.eu-central-1.amazonaws.com/kalisioscope/kaabah/kaabah-logo-black-128x128.png" />
-      </a>
-    </swiper-slide>
-    <swiper-slide>
-      <a href="https://kalisio.github.io/kargo/">
-        <img src="https://s3.eu-central-1.amazonaws.com/kalisioscope/kargo/kargo-logo-black-128x128.png" />
-      </a>
-    </swiper-slide>
-    <swiper-slide>
-      <a href="https://kalisio.gitbooks.io/krawler/">
-        <img src="https://s3.eu-central-1.amazonaws.com/kalisioscope/krawler/krawler-logo-black-128x128.png" />
-      </a>
-    </swiper-slide>
-    <swiper-slide>
-      <a href="https://weacast.gitbooks.io/weacast-docs/">
-        <img src="https://s3.eu-central-1.amazonaws.com/kalisioscope/weacast/weacast-logo-black-128x128.png" />
-      </a>
-    </swiper-slide>
-    <swiper-slide>
-      <a href="https://kalisio.gitbooks.io/kalisio/">
-        <img src="https://s3.eu-central-1.amazonaws.com/kalisioscope/kdk/kdk-logo-black-128x128.png" />
-      </a>
-    </swiper-slide>
-    <swiper-slide>
-      <a href="https:/aktnamp.com">
-        <img src="https://s3.eu-central-1.amazonaws.com/kalisioscope/aktnmap/aktnmap-logo-black-128x128.png" />
-      </a>
-    </swiper-slide>
-  </swiper>
+      <swiper-slide>
+        <a href="https://kalisio.com">
+          <img src="https://s3.eu-central-1.amazonaws.com/kalisioscope/kalisio/kalisio-logo-black-128x128.png" />
+        </a>
+      </swiper-slide>
+      <swiper-slide>
+        <a href="https://kalisio.github.io/kaabah/">
+          <img src="https://s3.eu-central-1.amazonaws.com/kalisioscope/kaabah/kaabah-logo-black-128x128.png" />
+        </a>
+      </swiper-slide>
+      <swiper-slide>
+        <a href="https://kalisio.github.io/kargo/">
+          <img src="https://s3.eu-central-1.amazonaws.com/kalisioscope/kargo/kargo-logo-black-128x128.png" />
+        </a>
+      </swiper-slide>
+      <swiper-slide>
+        <a href="https://kalisio.gitbooks.io/krawler/">
+          <img src="https://s3.eu-central-1.amazonaws.com/kalisioscope/krawler/krawler-logo-black-128x128.png" />
+        </a>
+      </swiper-slide>
+      <swiper-slide>
+        <a href="https://weacast.gitbooks.io/weacast-docs/">
+          <img src="https://s3.eu-central-1.amazonaws.com/kalisioscope/weacast/weacast-logo-black-128x128.png" />
+        </a>
+      </swiper-slide>
+      <swiper-slide>
+        <a href="https://kalisio.gitbooks.io/kalisio/">
+          <img src="https://s3.eu-central-1.amazonaws.com/kalisioscope/kdk/kdk-logo-black-128x128.png" />
+        </a>
+      </swiper-slide>
+      <swiper-slide>
+        <a href="https:/aktnamp.com">
+          <img src="https://s3.eu-central-1.amazonaws.com/kalisioscope/aktnmap/aktnmap-logo-black-128x128.png" />
+        </a>
+      </swiper-slide>
+    </swiper>
+  </ClientOnly>
 </template>
 
 <script>

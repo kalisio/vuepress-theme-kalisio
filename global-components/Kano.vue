191901885b100280d3a10755ada1f585f6f582f8
Christophe Nouguier <christophe.nouguier@free.fr>
2020-02-21 00:39:10 +0100

    Updated Kano.vue

--- global-components/Kano.vue
+++ global-components/Kano.vue
@@ -12,18 +12,18 @@
 </template>
 
 <script>
-import postRobot from 'post-robot'
+//import postRobot from 'post-robot'
 
 export default {
   name: 'kano',
   mounted() {
-    this.kano = document.getElementById('kano').contentWindow
+    /*this.kano = document.getElementById('kano').contentWindow
     this.component = 'map'
-    postRobot.on('kano-ready', () => {
+    /*postRobot.on('kano-ready', () => {
       postRobot.send(kano, 'setLocalStorage', {
         'kano-jwt': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJkZXYua2FsaXNpby54eXoiLCJpc3MiOiJrYWxpc2lvIiwiZXhwIjoxNTg0MDE1Mjg0fQ.oOPo_7lPMFgH6dBUtSnNs3w4h-DwwCSLdg9yMF0YU18'
       }).then(_ => postRobot.send(this.kano, this.component))
-    })
+    })*/
   }
 };
 </script>

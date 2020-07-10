b86d4bf987ec0f2af4e344b2cd8a130ec37e3171
Christophe Nouguier <christophe.nouguier@free.fr>
2020-07-10 15:41:10 +0200

    Added vuepress-plugin-server and vuepress-plugin-export

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ module.exports = {
   extend: '@vuepress/theme-default',
   plugins: [
     ['@vuepress/back-to-top', true],
-    ['@vuepress/back-to-top', true],
     ['@vuepress/pwa', {
       serviceWorker: true,
       updatePopup: true
-    }]
+    }],
+    ['vuepress-plugin-serve'],
+    ['vuepress-plugin-export']
   ]
 }

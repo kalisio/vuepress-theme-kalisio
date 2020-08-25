1193e3a25cfddbe3224af4ea00a58f173773d827
Luc Claustres <luc.claustres@orange.fr>
2020-08-25 13:23:42 +0200

    Added template url component

---
+++ global-components/TemplateUrl.vue
@@ -0,0 +1,39 @@
+<template>
+  <a target="_blank" :href="url">{{ text }}<i class="las la-external-link-square-alt"></i></a>
+</template>
+
+<script>
+
+export default {
+  name: 'template-url',
+  props: {
+    text: {
+      type: String,
+      default: ''
+    },
+    urlTemplate: {
+      type: String,
+      required: true
+    },
+    jwt: {
+      type: Boolean,
+      default: true
+    }
+  },
+  data () {
+    return {
+      url: ''
+    }
+  },
+  async mounted() {
+    const _ = await import('lodash')
+    const moment = await import('moment')
+    
+    const compiler = _.template(this.urlTemplate)
+    let context = { moment }
+    // Add any access token
+    context.jwt = this.$site.themeConfig.jwt
+    this.url = compiler(context)
+  }
+}
+</script>
